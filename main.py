58720de46081149fa694bf15308498fe6cd2322f
panchenkogit <odosaol@yandex.ru>
2025-02-08 18:46:43 +0500

    init app

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI(title="ExpenseTracker",
+              version="1.0")
+
+@app.get("/")
+async def hello() -> dict:
+    return {"message": 'hello'}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
